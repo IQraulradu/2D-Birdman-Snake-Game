b7beba306d833527fbd6873fadded2d8fb41884c
Raul <raduraul198@gmail.com>
2024-01-07 22:20:08 +0200

    first commit

---
+++ src/scenes/Credits.js
@@ -0,0 +1,21 @@
+
+import BaseScene from './BaseScene';
+
+class CreditsScene extends BaseScene {
+
+  constructor(config) {
+    super('CreditsScene', {...config, canGoBack: true});
+
+    this.menu = [
+      {scene: null, text: 'Thank you for playing'},
+      {scene: null, text: 'Author: Readu Raul'}
+    ]
+  }
+
+  create() {
+    super.create();
+    this.createMenu(this.menu, () => {});
+  }
+}
+
+export default CreditsScene;
